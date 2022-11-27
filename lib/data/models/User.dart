e30b49dc184cabc869fe6b9984bf23a3b864bcc1
ajomuch92 <aaronmontes_92@yahoo.es>
2022-11-27 17:08:43 -0600

    Updating user model

--- lib/data/models/User.dart
+++ lib/data/models/User.dart
@@ -1,7 +1,10 @@
+import "package:pocketbase/pocketbase.dart";
 class User {
-  String? id, name, email, username;
+  String? id, name, email, username, password, usernameOrEmail;
 
-  User({this.id, this.name, this.email, this.username});
+  User({this.id, this.name, this.email, this.username, this.password, this.usernameOrEmail});
+
+  factory User.fromRecord(RecordModel record) => User.fromJson(record.toJson());
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
@@ -9,6 +12,28 @@ class User {
       name: json['name'],
       email: json['email'],
       username: json['username'],
+      password: json['password']
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'username': username,
+      'password': password,
+    };
+  }
+
+  Map<String, dynamic> toJsonSignUp() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'username': username,
+      'password': password,
+      'passwordConfirm': password,
+    };
   }
 }

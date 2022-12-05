fd8bb3bffeb5e68ac3a79015e01e3f03b91701f8
ajomuch92 <aaronmontes_92@yahoo.es>
2022-12-04 20:39:38 -0600

    Updating Tab1 and GameCard widgets

--- lib/presentation/views/Home/Tab1.dart
+++ lib/presentation/views/Home/Tab1.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:getwidget/components/accordion/gf_accordion.dart';
 import 'package:getwidget/getwidget.dart';
 import 'package:quiniela_hn_app/data/models/Game.dart';
 import 'package:quiniela_hn_app/data/models/GameDay.dart';
+import 'package:quiniela_hn_app/data/models/Settings.dart';
 import 'package:quiniela_hn_app/domain/home/HomeProvider.dart';
+import 'package:quiniela_hn_app/domain/home/SettingsProvider.dart';
 import 'package:quiniela_hn_app/presentation/views/Home/Widgets/Game.dart';
 
 class Tab1 extends ConsumerStatefulWidget {
@@ -24,22 +25,54 @@ class _Tab1State extends ConsumerState<Tab1> {
   @override
   Widget build(BuildContext context) {
     List<GameDay> gameDayList = ref.watch(gamesProvider);
-    return ListView.separated(
-      itemBuilder: (BuildContext context, int index) {
-        GameDay gameDay = gameDayList[index];
-        List<Game> games = gameDay.games;
-        return GFAccordion(
-          showAccordion: false,
-          title: gameDay.name,
-          contentChild: SingleChildScrollView(
-            child: Column(
-              children: games.map((e) => GameView(game: e)).toList(),
+    Settings settings = ref.watch(settingProvider);
+    String currentGameDayId = ref.watch(currentGameIdProvider);
+    if (currentGameDayId.isEmpty) currentGameDayId = settings.currentGameDayId!;
+    List<Game> games = ref.watch(gameListProvider);
+    if (games.isEmpty) {
+      int index = gameDayList.indexWhere((element) => element.id == currentGameDayId);
+      games = index != -1 ? gameDayList[index].games : [];
+    }
+    return Column(
+      children: [
+        const SizedBox(height: 10,),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.9,
+          height: 50,
+          child: DropdownButtonHideUnderline(
+            child: GFDropdown(
+              padding: const EdgeInsets.all(15),
+              borderRadius: BorderRadius.circular(5),
+              border: const BorderSide(color: Colors.black12, width: 1),
+              dropdownButtonColor: Colors.white,
+              value: currentGameDayId,
+              onChanged: (newValue) {
+                String newValueString = newValue as String;
+                ref.read(currentGameIdProvider.notifier).setText(newValueString);
+                int index = gameDayList.indexWhere((element) => element.id == newValueString);
+                ref.read(gameListProvider.notifier).setList(index != -1 ? gameDayList[index].games : []);
+              },
+              items: gameDayList
+                  .map((value) => DropdownMenuItem(
+                value: value.id,
+                child: Text(value.name!),
+              )).toList(),
             ),
-          )
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) => const Divider(), 
-      itemCount: gameDayList.length
+          ),
+        ),
+        const SizedBox(height: 10,),
+        games.isNotEmpty ? Expanded(
+          child: ListView.separated(
+            itemBuilder: (BuildContext context, int index) {
+              Game currentGame = games[index];
+              return GameCard(game: currentGame, key: Key(currentGame.id!),);
+            },
+            separatorBuilder: (BuildContext context, int index) => const Divider(), 
+            itemCount: games.length,
+          ),
+        ) :
+        const Text('Selecciones una jornada para mostrar sus partidos', style: TextStyle(color: Colors.black87), textAlign: TextAlign.center,)
+      ],
     );
   }
 }
--- lib/presentation/views/Home/Widgets/Game.dart
+++ lib/presentation/views/Home/Widgets/Game.dart
@@ -1,21 +1,23 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_spinbox/material.dart';
+import 'package:getwidget/components/card/gf_card.dart';
 import 'package:intl/intl.dart';
 import 'package:quiniela_hn_app/data/models/Game.dart';
 import 'package:quiniela_hn_app/domain/home/HomeProvider.dart';
 import 'package:quiniela_hn_app/presentation/utils/Debouncer.dart';
 import 'package:quiniela_hn_app/presentation/utils/index.dart';
 
-class GameView extends ConsumerStatefulWidget {
+class GameCard extends ConsumerStatefulWidget {
   final Game game;
-  const GameView({Key? key, required this.game}) : super(key: key);
+  const GameCard({Key? key, required this.game}) : super(key: key);
 
   @override
-  ConsumerState<GameView> createState() => _GameViewState();
+  ConsumerState<GameCard> createState() => _GameViewState();
 }
 
-class _GameViewState extends ConsumerState<GameView> {
+class _GameViewState extends ConsumerState<GameCard> {
   late Game game;
   final f = DateFormat('dd/MM/yyyy hh:mm a');
   Debouncer debouncer1 = Debouncer(milliseconds: 300);
@@ -39,104 +41,129 @@ class _GameViewState extends ConsumerState<GameView> {
     DateTime now = ref.watch(timerProvider);
     bool isLessThan5Minutes = game.dateTime != null ?  game.dateTime!.difference(now).inMinutes < 5: false;
     bool isReadyToMakePrediction = game.status == 'not played';
-    return Column(
-      children: [
-        Row(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Text(
-                game.homeTeam!.name!,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              )
-            ),
-            Expanded(flex: 1, child: Container(),),
-            Expanded(
-              flex: 2,
-              child: Text(
-                game.awayTeam!.name!,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontWeight: FontWeight.bold),
+    return GFCard(
+      content: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Text(
+                  game.homeTeam!.name!,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                )
               ),
-            )
-          ],
-        ),
-        Row(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Image.network(game.homeTeam!.image!),
-            ),
-            const Expanded(flex: 1, child: Center(child: Text('VS'),)),
-            Expanded(
-              flex: 2,
-              child: Image.network(game.awayTeam!.image!),
-            )
-          ],
-        ),
-        !isLessThan5Minutes && isReadyToMakePrediction ?
-        Row(
-          children: [
-            Expanded(
-              flex: 2,
-              child: SpinBox(
-                min: 0,
-                max: 15,
-                value: double.tryParse(game.prediction!.homeScore.toString()) ?? 0,
-                onChanged: (value) {
-                  debouncer1.run(() {
-                    game.prediction!.homeScore = value.toInt();
-                    savePrediction(context);
-                  });
-                },
+              Expanded(flex: 1, child: Container(),),
+              Expanded(
+                flex: 2,
+                child: Text(
+                  game.awayTeam!.name!,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
               )
-            ),
-            Expanded(
-              flex: 2,
-              child: SpinBox(
-                min: 0,
-                max: 15,
-                value: double.tryParse(game.prediction!.awayScore.toString()) ?? 0,
-                onChanged: (value) {
-                  debouncer2.run(() {
-                    game.prediction!.awayScore = value.toInt();
-                    savePrediction(context);
-                  });
-                },
-              )
-            )
-          ],
-        ) :
-        Row(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Text(
-                game.prediction!.homeScore.toString(),
-                textAlign: TextAlign.center,
-              )
-            ),
-            Expanded(flex: 1, child: Container(),),
-            Expanded(
-              flex: 2,
-              child: Text(
-                game.prediction!.awayScore.toString(),
-                textAlign: TextAlign.center,
-              ),
-            )
-          ],
-        ),
-        Center(
-          child: Chip(
-            label: Text('${game.homeScore} - ${game.awayScore}'), 
-            backgroundColor: game.status == 'playing' ? Colors.orangeAccent : Colors.lightBlueAccent
+            ],
           ),
-        ),
-        Text(game.gameType!.type!),
-        Text(game.stadium ?? 'Estadio sin especificar'),
-        Text(f.format(game.dateTime!)),
-      ],
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: SizedBox(
+                  width: 100, 
+                  height: 100,
+                  child: CachedNetworkImage(
+                      imageUrl:  game.homeTeam!.image!,
+                      placeholder: (context, url) => const LinearProgressIndicator(),
+                      errorWidget: (context, url, error) => const Icon(Icons.error, color: Colors.red,),
+                  ),
+                ),
+              ),
+              const Expanded(flex: 1, child: Center(child: Text('VS'),)),
+              Expanded(
+                flex: 2,
+                child: SizedBox(
+                  width: 100, 
+                  height: 100, 
+                  child: CachedNetworkImage(
+                      imageUrl:  game.awayTeam!.image!,
+                      placeholder: (context, url) => const LinearProgressIndicator(),
+                      errorWidget: (context, url, error) => const Icon(Icons.error, color: Colors.red,),
+                  ),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 10,),
+          !isLessThan5Minutes && isReadyToMakePrediction ?
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: SpinBox(
+                  min: 0,
+                  max: 15,
+                  value: double.tryParse(game.prediction!.homeScore.toString()) ?? 0,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                  onChanged: (value) {
+                    debouncer1.run(() {
+                      game.prediction!.homeScore = value.toInt();
+                      savePrediction(context);
+                    });
+                  },
+                )
+              ),
+              Expanded(
+                flex: 2,
+                child: SpinBox(
+                  min: 0,
+                  max: 15,
+                  value: double.tryParse(game.prediction!.awayScore.toString()) ?? 0,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                  onChanged: (value) {
+                    debouncer2.run(() {
+                      game.prediction!.awayScore = value.toInt();
+                      savePrediction(context);
+                    });
+                  },
+                )
+              )
+            ],
+          ) :
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Text(
+                  game.prediction!.homeScore.toString(),
+                  textAlign: TextAlign.center,
+                )
+              ),
+              Expanded(flex: 1, child: Container(),),
+              Expanded(
+                flex: 2,
+                child: Text(
+                  game.prediction!.awayScore.toString(),
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
+          Center(
+            child: Chip(
+              label: Text('${game.homeScore} - ${game.awayScore}'), 
+              backgroundColor: game.status == 'playing' ? Colors.orangeAccent : Colors.lightBlueAccent
+            ),
+          ),
+          Text(game.gameType!.type!),
+          Text(game.stadium ?? 'Estadio sin especificar'),
+          Text(f.format(game.dateTime!)),
+        ],
+      ),
     );
   }
 

b563acb140918c297820399560d96ac87d435b4d
Aarón J. Montes <aaronmontes_92@yahoo.es>
2024-05-27 21:50:53 -0600

    feat: 🎸 Adding method to get my user

--- lib/services/AuthService.dart
+++ lib/services/AuthService.dart
@@ -1,5 +1,6 @@
 import 'package:pocketbase/pocketbase.dart';
 import 'package:quiniela_hn_app/database/index.dart';
+import 'package:quiniela_hn_app/models/User.dart';
 
 Future<RecordAuth> tryToLogin(String email, String password) async {
   return await pb.collection('users').authWithPassword(email, password);
@@ -11,4 +12,8 @@ logout() {
 
 Future<void> trySignUp(Map<String, dynamic> payload) async {
   await pb.collection('users').create(body: payload);
+}
+
+User getMyUser() {
+  return User.fromRecordModal(pb.authStore.model);
 }
